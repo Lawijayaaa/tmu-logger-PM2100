84f1b21ea9409dc135d5916c2ee943ddb6306bb1
Lawijayaaa <lawijaya.lutfi@gmail.com>
2025-05-18 16:27:06 +0700

    Edit SerialTest

--- test/SerialTest.py
+++ test/SerialTest.py
@@ -4,26 +4,22 @@ import time
 
 #init modbus device
 client = ModbusSerialClient(method='rtu', port='/dev/ttyACM0', baudrate=9600)
-client2 = ModbusSerialClient(method='rtu', port='/dev/ttyUSB1', baudrate=9600)
 loop = False
 
 def testBatch():
-    #writeRly = client.write_coil(1, True, slave = 1)
-    #writeRly = client.write_coil(2, True, slave = 1)
-    #writeRly = client.write_coil(3, True, slave = 1)
-    #writeRly = client.write_coil(4, True, slave = 1)
-    #writeRly = client.write_coil(5, True, slave = 1)
-    #print(writeRly)
+    getElect1 = client.read_holding_registers(2999, 8, slave = 2)
+    getElect2 = client.read_holding_registers(3019, 14, slave = 2)
+    getElect3 = client.read_holding_registers(3053, 24, slave = 2)
+    getElect4 = client.read_holding_registers(3077, 18, slave = 2)
+    getElect5 = client.read_holding_registers(3213, 20, slave = 2)
+    getElect6 = client.read_holding_registers(21299, 24, slave = 2)
     
-    getElect = client.read_holding_registers(0, 29, slave = 2)
-    print(getElect.registers)
-    getTemp = client.read_holding_registers(4, 3, slave = 3)   
-    print(getTemp.registers) 
-    getH2 = client2.read_holding_registers(0, 10, slave = 4)
-    print(getH2.registers)
-    getMoist = client2.read_input_registers(0, 3, slave = 5)
-    print(getMoist.registers[0])
-    print(getMoist.registers[2])
+    print(getElect1)
+    print(getElect2)
+    print(getElect3)
+    print(getElect4)
+    print(getElect5)
+    print(getElect6)
     
     print("~~~")
 
